7ba34cf43cad413ea8625aaab7d8d6c805d0766a
OneGoodReas0n <korobovi4@gmail.com>
2019-10-04 21:45:04 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -37,8 +37,36 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const BYNARY_TO_MORSE = {
+    '10': '.',
+    '11': '-',
+}
+
+function binaryToMorse(str) {
+    let morseLetter = "";
+    const morseSignsBinaryArray = String(str).match(/.{1,2}/g);
+    for (let i = 0; i < morseSignsBinaryArray.length; i++) {
+        morseLetter += BYNARY_TO_MORSE[morseSignsBinaryArray[i]]
+    }
+    return morseLetter;
+}
+
 function decode(expr) {
     // write your solution here
+    let decodeStr = "";
+    const signsMassive = String(expr).match(/.{1,10}/g);
+    for (let i = 0; i < signsMassive.length; i++) {
+        if (String(signsMassive[i]).indexOf('1') !== -1) {
+            const firstPositionOfElement = String(signsMassive[i]).indexOf('1');
+            const letterInBinary = String(signsMassive[i]).substr(firstPositionOfElement)
+            const morseLetter = binaryToMorse(letterInBinary);
+            decodeStr += MORSE_TABLE[morseLetter]
+        }
+        else{
+            decodeStr += " ";
+        }
+    }
+    return decodeStr;
 }
 
 module.exports = {
